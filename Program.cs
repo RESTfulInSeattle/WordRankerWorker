41f50acb73e20a78939dae38c35ee7b9ce9c0271
David Clayton <david@davidclayton.me>
2018-02-13 19:21:39 -0800

    Created Project

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Amazon.SQS.Model;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WordRankerWorker
+{
+    class Program
+    {
+        public static string Rank(string url)
+        {
+            if (url.Length > 0)
+            {
+                URLFetcher uf = new URLFetcher();
+                string urlText = uf.GetURLText(url);
+
+                if (urlText.Length > 0)
+                {
+                    string returnString;
+                    WordCollector wc = new WordCollector();
+                    char[] delimeters = { ' ' };
+                    Dictionary<string, long> words = wc.GetWords(urlText, delimeters);
+
+                    if (words.Keys.Count > 0)
+                    {
+                        WordRanker wr = new WordRanker();
+                        List<KeyValuePair<string, long>> rankedWords = wr.RankWords(words, 10);
+
+                        returnString = "Top 10 Words:" + "\r\n";
+
+                        foreach (KeyValuePair<string, long> word in rankedWords)
+                        {
+                            returnString += $"Word: {word.Key}  Count: {word.Value}" + "\r\n";
+                        }
+
+                        return returnString;
+                    }
+                    else
+                    {
+                        return "No words retrieved from URL: " + url;
+                    }
+                }
+                else
+                {
+                    return "No text retrieved from URL: " + url;
+                }
+
+
+            }
+            else
+            {
+                return "Invalid URL";
+            }
+        }
+
+        public static void CheckForWork()
+        {
+            ReceiveMessageResponse messages =
+                SQSActions.GetMessages("https://sqs.us-west-2.amazonaws.com/792614619693/Assignment3-Queue");
+
+            if (messages.Messages.Count > 0)
+            {
+                foreach (var message in messages.Messages)
+                {
+                    string returnRank = Rank(message.Body);
+
+                    bool success = DynamoDbActions.AddItem(message.Body, returnRank);
+
+                    success = SQSActions.DeleteMessage("https://sqs.us-west-2.amazonaws.com/792614619693/Assignment3-Queue", message.ReceiptHandle);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                CheckForWork();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ SQSActions.cs
@@ -0,0 +1,61 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Net;
+
+namespace WordRankerWorker
+{
+    public class SQSActions
+    {
+
+        private static string serviceURL = "http://sqs.us-west-2.amazonaws.com";
+
+        public static ReceiveMessageResponse GetMessages(string queueURL)
+        {
+
+            var sqsConfig = new AmazonSQSConfig {ServiceURL = serviceURL};
+            var sqsClient = new AmazonSQSClient(sqsConfig);
+
+            var receiveMessageRequest = new ReceiveMessageRequest {QueueUrl = queueURL};
+
+            var receiveMessageResponse = sqsClient.ReceiveMessage(receiveMessageRequest);
+            return receiveMessageResponse;
+        }
+
+        public static bool DeleteMessage(string queueURL, string recieptHandle)
+        {
+            var sqsConfig = new AmazonSQSConfig {ServiceURL = serviceURL};
+            var sqsClient = new AmazonSQSClient(sqsConfig);
+
+            var deleteMessageRequest = new DeleteMessageRequest
+            {
+                QueueUrl = queueURL,
+                ReceiptHandle = recieptHandle
+            };
+
+            var response = sqsClient.DeleteMessage(deleteMessageRequest);
+
+            if (response.HttpStatusCode == HttpStatusCode.Accepted) return true;
+
+            return false;
+        }
+
+        public static bool AddMessage(string queueURL, string messageBody)
+        {
+            var sqsConfig = new AmazonSQSConfig { ServiceURL = serviceURL };
+            var sqsClient = new AmazonSQSClient(sqsConfig);
+
+            var sendMessageRequest = new SendMessageRequest
+            {
+                MessageBody = messageBody,
+                QueueUrl = queueURL
+            };
+
+            var response = sqsClient.SendMessage(sendMessageRequest);
+
+            if (response.HttpStatusCode == HttpStatusCode.Accepted) return true;
+
+            return false;
+        }
+        
+    }
+}
